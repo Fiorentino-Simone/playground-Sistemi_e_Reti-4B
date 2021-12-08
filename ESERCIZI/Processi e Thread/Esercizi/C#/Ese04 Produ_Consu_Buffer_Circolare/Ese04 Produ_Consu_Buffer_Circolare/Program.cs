4f6b2312e05aa7b93dfe1b4b0f5d2dc7321d2952
Fiorentino Simone <s.fiorentino.1743@vallauri.edu>
2021-12-08 22:46:36 +0100

    Completed test 1 about product and consumer

--- ESERCIZI/Processi e Thread/Esercizi/C#/Ese04 Produ_Consu_Buffer_Circolare/Ese04 Produ_Consu_Buffer_Circolare/Program.cs
+++ ESERCIZI/Processi e Thread/Esercizi/C#/Ese04 Produ_Consu_Buffer_Circolare/Ese04 Produ_Consu_Buffer_Circolare/Program.cs
@@ -1,15 +1,90 @@
-﻿using System;
+﻿///////////////////////////////////////////////////////////////////////////FIORENTINO SIMONE 4^B /////////////////////////////////////////////////////////////////////////////////////////////
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Ese04_Produ_Consu_Buffer_Circolare
 {
     class Program
     {
+        const int numDati = 10; //
+        const int tempoProduzione = 200; // [ms]
+        const int tempoConsumo = 500; // [ms]
+        const int MAX_DIM = 10; 
+        static SemaphoreSlim vuoto = new SemaphoreSlim(1);
+        static SemaphoreSlim pieno = new SemaphoreSlim(0);
+        static SemaphoreSlim space = new SemaphoreSlim(MAX_DIM); //semaforo che contiene il numero di spazi
+        static int[] buffer = new int[MAX_DIM];
+        static Random rnd = new Random(); 
+
         static void Main(string[] args)
         {
+
+            //CONSEGNA: 
+
+            /*
+             *  Un produttore, un consumatore, buffer circolare: 
+                Partendo dall’esercitazione precedente sviluppare una soluzione gestendo un buffer circolare =>
+                static int[] buffer = new int[dimBuffer];
+            */
+
+            Thread produttore = new Thread(Produttore);
+            Thread consumatore = new Thread(Consumatore);
+            Console.WriteLine("INIZIO PROGRAMMA: " + DateTime.Now.ToString("hh:mm:ss,fff"));
+
+            produttore.Start();
+            consumatore.Start();
+            //Parallel.Invoke(Produttore, Consumatore); //la procedura invoke permette di assegnare le funzioni a thread singoli, quindi le esegue parallelamente.
+
+            //li riuniamo, bloccando il thread
+            produttore.Join();
+            consumatore.Join();
+
+
+            Console.WriteLine("FINE PROGRAMMA: " + DateTime.Now.ToString("hh:mm:ss,fff"));
+            Console.ReadKey();
+        }
+
+        static void Produttore()
+        {
+            int i = 0;
+            int put = 0; //indice interno usato per aggiornare il buffer dopo aver prodotto il messaggio
+            while (i != numDati)
+            {
+                space.Wait();
+                vuoto.Wait();
+                // INIZIO SEZIONE CRITICA
+                buffer[put] = rnd.Next(1, 500);
+                Console.WriteLine("Messaggio creato, tempo: " + DateTime.Now.ToString("hh:mm:ss:fff") + "): " + buffer[put].ToString());
+                put = (put + 1) % MAX_DIM; //aggiorniamo l'indice
+                vuoto.Release();
+                pieno.Release();
+                //FINE SEZIONE CRITICA
+                Thread.Sleep(tempoProduzione); //impostiamo la sleep
+                i++;
+            }
+        }
+
+        static void Consumatore()
+        {
+            int i = 0;
+            int get = 0; //indice interno usato per aggiornare il buffer dopo aver consumato il messaggio
+            while (i != numDati)
+            {
+                pieno.Wait();
+                vuoto.Wait();
+                // INIZIO SEZIONE CRITICA
+                Console.WriteLine(" - Messaggio ricevuto, tempo: " + DateTime.Now.ToString("hh:mm:ss:fff") + "): " + buffer[get].ToString());
+                get = (get + 1) % MAX_DIM; //aggiorniamo l'indice
+                vuoto.Release();
+                space.Release();
+                //FINE SEZIONE CRITICA
+                Thread.Sleep(tempoConsumo); //impostiamo la sleep
+                i++;
+            }
         }
     }
 }
---
+++ ESERCIZI/Processi e Thread/Esercizi/C#/Ese05 Produ_Consu_Singolo/Ese05 Produ_Consu_Singolo/Program.cs
@@ -0,0 +1,92 @@
+﻿//////////////////////////////////////////////////////////////FIORENTINO SIMONE 4^B //////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ese05_Produ_Consu_Singolo
+{
+    class Program
+    {
+        const int numDati = 10; //
+        const int tempoProduzione = 200; // [ms]
+        const int tempoConsumo = 500; // [ms]
+        static SemaphoreSlim vuoto = new SemaphoreSlim(1);
+        static SemaphoreSlim pieno = new SemaphoreSlim(0);
+        static int buffer;
+        static Random rnd = new Random(); //utilizzato per generare randomicamente il valore nel buffer
+        static void Main(string[] args)
+        {
+            //CONSEGNA: 
+
+            /*
+             * Sviluppare un programma C# (Console .NET Framework) che gestisca la produzione (scrittura) e
+                consumo (lettura/modifica) del contenuto di una variabile (buffer)
+                Massimizzare l’efficienza del programma utilizzando opportunamente l’oggetto Thread e, se si
+                ritiene opportuno, l’oggetto Parallel.
+                Nello specifico, all’avvio del programma avverrà una prima fase di ‘produzione’ dove,
+                ipoteticamente, verrà scritto all’interno del buffer (per un numero limitato di iterazioni) un valore
+                generato casualmente.
+                Sarà compito del consumatore, leggere il contenuto e, solo successivamente, dare la possibilità
+                al produttore di crearne uno nuovo.
+                Gestire la mutua esclusione utilizzando i semafori necessari (oggetto SemaphoreSlim).
+                Ovviamente, ogni qual volta viene eseguita una operazione di produzione/consumo, i rispettivi
+                thread attendono qualche istante (Sleep).
+                PER VERIFICARE L’EFFICIENZA DEL CODICE STAMPARE OGNI PASSAGGIO DI
+                PRODUZIONE/CONSUMO. INCLUDENDO ANCHE IL TEMPO DI ELABORAZIONE DEL THREAD
+                PRINCIPALE (Main).
+            */
+
+            Thread.CurrentThread.Name = "_main_";
+            Thread produttore = new Thread(Produttore);
+            Thread consumatore = new Thread(Consumatore);
+            Console.WriteLine("INIZIO PROGRAMMA: " + DateTime.Now.ToString("hh:mm:ss,fff"));
+
+            produttore.Start();
+            consumatore.Start();
+            //Parallel.Invoke(Produttore, Consumatore); //la procedura invoke permette di assegnare le funzioni a thread singoli, quindi le esegue parallelamente.
+
+            //li riuniamo, bloccando il thread
+            produttore.Join();
+            consumatore.Join();
+            
+            
+            Console.WriteLine("FINE PROGRAMMA: " + DateTime.Now.ToString("hh:mm:ss,fff"));
+            Console.WriteLine("Buffer: " + buffer.ToString());
+            Console.ReadKey();
+        }
+
+        static void Produttore()
+        {
+            int i = 0;
+            while (i < numDati)
+            {
+                vuoto.Wait();
+                // INIZIO SEZIONE CRITICA
+                buffer = rnd.Next(1, 500);
+                Console.Write("Messaggio creato, tempo: " + DateTime.Now.ToString("hh:mm:ss:fff") + "): " + buffer);
+                pieno.Release();
+                //FINE SEZIONE CRITICA
+                Thread.Sleep(tempoProduzione); //impostiamo la sleep
+                i++;
+            }
+        }
+
+        static void Consumatore()
+        {
+            int i = 0;
+            while (i < numDati)
+            {
+                pieno.Wait();
+                // INIZIO SEZIONE CRITICA
+                Console.WriteLine(" - Messaggio ricevuto, tempo: " + DateTime.Now.ToString("hh:mm:ss:fff") + "): " + buffer);
+                vuoto.Release();
+                //FINE SEZIONE CRITICA
+                Thread.Sleep(tempoConsumo); //impostiamo la sleep
+                i++;
+            }
+        }
+    }
+}
