e1786ea704de717270b04c217f8d989ff8a33ab7
Stud. Simone Fiorentino <s.fiorentino.1743@vallauri.net>
2021-12-15 09:56:34 +0100

    lesson 5: protocolli e modelli

---
+++ ESERCIZI/Processi e Thread/Esercizi/C#/Ese07 Form_Editor/Ese07 Form_Editor/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Ese07_Form_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.lblCaratteri = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Location = new System.Drawing.Point(2, 42);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(796, 396);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            this.rtb.KeyUp += new System.Windows.Forms.KeyEventHandler(this.rtb_KeyUp);
+            // 
+            // lblCaratteri
+            // 
+            this.lblCaratteri.AutoSize = true;
+            this.lblCaratteri.Location = new System.Drawing.Point(13, 23);
+            this.lblCaratteri.Name = "lblCaratteri";
+            this.lblCaratteri.Size = new System.Drawing.Size(52, 13);
+            this.lblCaratteri.TabIndex = 1;
+            this.lblCaratteri.Text = "Caratteri: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCaratteri);
+            this.Controls.Add(this.rtb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.Label lblCaratteri;
+    }
+}
+
---
+++ ESERCIZI/Processi e Thread/Esercizi/C#/Ese07 Form_Editor/Ese07 Form_Editor/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Ese07_Form_Editor
+{
+    public partial class Form1 : Form
+    {
+        static Thread tConta, tSalva;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr;
+            /* Caricamento del contenuto da file */
+            if (!File.Exists("miofile.txt")) File.Create("miofile.txt");
+
+            sr = new StreamReader("miofile.txt");
+            rtb.Text = sr.ReadToEnd();
+            sr.Close();
+            lblCaratteri.Text = "Caratteri: " + rtb.Text.Trim().Length;
+
+            //il conteggio e il salvataggio devono essere gestiti in sezione critica
+            tConta = new Thread(avviaConteggio);
+            tSalva = new Thread(avviaSalvataggio);
+        }
+
+        private void rtb_KeyUp(object sender, KeyEventArgs e)
+        {
+            //uguale al textChanged semplicemente questo evento viene richiamato quando viene premuto un tasto
+
+            //GESTIONE SEZIONE CRITICA (SALVATAGGIO E CONTEGGIO)
+            tConta.Start();
+            tSalva.Start();
+        }
+
+        private void avviaSalvataggio()
+        {
+            //salvataggio file
+            this.Invoke(new EventHandler(salva));
+            
+        }
+
+        private void salva(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("miofile.txt");
+            sw.Write(rtb.Text); //bisogna passare dai delegated siccome il thread principale al compito di gestire UNIVOCAMENTE tutti gli elementi grafici della pagina.
+            sw.Close();
+        }
+
+        private void avviaConteggio()
+        {
+            //conteggio dei caratteri
+            this.Invoke(new EventHandler(conta)); //bisogna usare il delegated tramite l'invoke della form
+        }
+
+        private void conta(object sender, EventArgs e)
+        {
+            lblCaratteri.Text = "Caratteri: " + rtb.Text.Trim().Length;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            /* Visualizzo directory contenente il mio file*/
+
+        }
+    }
+}
