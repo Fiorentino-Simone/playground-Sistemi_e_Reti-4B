b1a8bbd09d5990bc21b982172ae62ac5ebee5f42
Fiorentino-Simone <s.fiorentino.1743@vallauri.edu>
2022-04-30 22:12:49 +0200

    Create two exercises about TCP/UDP Server and begin Modulo 11 on CISCO

---
+++ ESERCIZI/SOCKET/Ese02 Socket TCP Server/Ese02 Socket TCP Server/ClsTCPServer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ese02_Socket_TCP_Server
+{
+    //Definiamo firma procedura di Eventi datiRicevutiHandler
+    public delegate void datiRicevutiHandler(clsMessage clsMess);
+
+    //Definiamo firma procedura di Evento erroreConnessioneEvent --> essendo TCP (ad esempio ho la porta oppure l'indirizzo IP sbagliato)
+    public delegate void erroreConnessioneEventHandler(string msg);
+
+    //Definiamo lo status della connessione siccome potrebbe avere un calo. Essendo con un protocollo TCP dobbiamo controllarlo
+    public delegate void statoConnessioneEventHandler(string msg);
+
+    internal class ClsTCPServer
+    {
+        const int MAX_BYTE = 1024; //lunghezza max buffer
+        const int MAX_CONN = 20; //max numero di connessioni client accettati
+        private Socket socketID;
+        private EndPoint endPoint; //socket (ip, porta) associato al server
+        private Thread threadAscolto;
+        private volatile bool threadRun = true;
+        private Socket connID; 
+
+        public event datiRicevutiHandler datiRicevutiEvent;
+        public event erroreConnessioneEventHandler erroreConnessioneEvent;   
+        public event statoConnessioneEventHandler statoConnessioneEvent; 
+    
+        public ClsTCPServer(IPAddress ip, int porta)
+        {
+            socketID = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            endPoint = new IPEndPoint(ip, porta);
+
+            socketID.SendTimeout = 10000; //assegniamo il timeout di invio e di ricezione 
+            socketID.ReceiveTimeout = 10000;
+
+            socketID.Bind(endPoint); //mettiamo in ascolto su porta specifica
+            socketID.Listen(MAX_CONN); // alla funzione listen specifichiamo il numero massimo di connessioni accettate prima di restituire "Server Occupato"
+        }
+
+        public void avvia()
+        {
+            if (threadAscolto == null)
+            {
+                threadAscolto = new Thread(ricevi);
+                threadAscolto.Start();
+                while (!threadAscolto.IsAlive) ;//iteriamo all'infinito finche thread non è avviato
+            }
+        }
+
+        private void termina()
+        {
+            socketID.Close();   
+            threadRun = false;
+        }
+
+        public void invia(string msg)
+        {
+            try
+            {
+                byte[] bufferTx;
+                bufferTx = Encoding.UTF8.GetBytes(msg);
+                connID.Send(bufferTx, bufferTx.Length, SocketFlags.None);
+            }
+            catch (SocketException ex)
+            {
+                erroreConnessioneEvent(ex.Message);
+            }
+        }
+
+        private void ricevi()
+        {
+            byte[] bufferRx = new byte[MAX_BYTE];
+            string msg;
+            int nByteRicevuti = 0;
+            bool esci; 
+
+            while (threadRun)
+            {
+                try
+                {
+                    if (connID == null)
+                    {
+                        //socketID --> si pone in ascolto, però quando riceve una richiesta di connessione passa tutto a connId
+                        connID = socketID.Accept(); //va ad instanziare connId, ogni volta che abbiamo una richiesta diversa
+                        statoConnessioneEvent("Connesso");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    erroreConnessioneEvent(ex.Message);
+                    chiudiConnessione();
+                }
+            }
+        }
+
+        public void chiudiConnessione()
+        {
+            try
+            {
+                connID.Shutdown(SocketShutdown.Both);
+                connID.Close();
+                connID = null; //rimuovo l'istanza
+                statoConnessioneEvent("Disconnesso");
+            }
+            catch (Exception ex)
+            {
+                erroreConnessioneEvent(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ ESERCIZI/SOCKET/Ese02 Socket TCP Server/Ese02 Socket TCP Server/frmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Ese02_Socket_TCP_Server
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpIp = new System.Windows.Forms.GroupBox();
+            this.cmbServer = new System.Windows.Forms.ComboBox();
+            this.gpPort = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtRisultato = new System.Windows.Forms.TextBox();
+            this.gpIp.SuspendLayout();
+            this.gpPort.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gpIp
+            // 
+            this.gpIp.Controls.Add(this.cmbServer);
+            this.gpIp.Location = new System.Drawing.Point(35, 30);
+            this.gpIp.Name = "gpIp";
+            this.gpIp.Size = new System.Drawing.Size(200, 100);
+            this.gpIp.TabIndex = 0;
+            this.gpIp.TabStop = false;
+            this.gpIp.Text = "IP";
+            // 
+            // cmbServer
+            // 
+            this.cmbServer.FormattingEnabled = true;
+            this.cmbServer.Location = new System.Drawing.Point(7, 37);
+            this.cmbServer.Name = "cmbServer";
+            this.cmbServer.Size = new System.Drawing.Size(187, 24);
+            this.cmbServer.TabIndex = 0;
+            // 
+            // gpPort
+            // 
+            this.gpPort.Controls.Add(this.numericUpDown1);
+            this.gpPort.Location = new System.Drawing.Point(260, 30);
+            this.gpPort.Name = "gpPort";
+            this.gpPort.Size = new System.Drawing.Size(200, 100);
+            this.gpPort.TabIndex = 1;
+            this.gpPort.TabStop = false;
+            this.gpPort.Text = "PORT";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(7, 37);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            49151,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(172, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Red;
+            this.btnStart.Location = new System.Drawing.Point(477, 49);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(143, 59);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Lime;
+            this.btnStop.Location = new System.Drawing.Point(626, 49);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(143, 59);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            // 
+            // txtRisultato
+            // 
+            this.txtRisultato.Location = new System.Drawing.Point(35, 137);
+            this.txtRisultato.Multiline = true;
+            this.txtRisultato.Name = "txtRisultato";
+            this.txtRisultato.Size = new System.Drawing.Size(425, 256);
+            this.txtRisultato.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtRisultato);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.gpPort);
+            this.Controls.Add(this.gpIp);
+            this.Name = "frmMain";
+            this.Text = "SERVER TCP";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.gpIp.ResumeLayout(false);
+            this.gpPort.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpIp;
+        private System.Windows.Forms.ComboBox cmbServer;
+        private System.Windows.Forms.GroupBox gpPort;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtRisultato;
+    }
+}
+
